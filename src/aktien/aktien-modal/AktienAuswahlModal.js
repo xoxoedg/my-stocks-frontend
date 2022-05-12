79b664b75677da8676c2212e06b77b77f3a9a372
TehseenRug <tehseen.rug@gmail.com>
2022-05-12 21:21:32 +0200

    toggle aktien selection

--- src/aktien/aktien-modal/AktienAuswahlModal.js
+++ src/aktien/aktien-modal/AktienAuswahlModal.js
@@ -4,17 +4,18 @@ import DialogActions from '@mui/material/DialogActions';
 import DialogContent from '@mui/material/DialogContent';
 import DialogTitle from '@mui/material/DialogTitle';
 import {useEffect, useState} from "react";
-import {Checkbox, TableCell} from "@mui/material";
+import {Checkbox, FormControlLabel, FormGroup, TableCell} from "@mui/material";
 import Paper from "@mui/material/Paper";
 import Table from "@mui/material/Table";
-import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import TableBody from "@mui/material/TableBody";
 import TableContainer from "@mui/material/TableContainer";
 import RequestHandler from "../RequestHandler";
+import SelectedAktie from "../structs/AktienStructs";
 
 function AktienAuswahlModal(props) {
     const [aktienAuswahl, setAktienAuswahl] = useState([])
+    const [selectedAktien, setSelectedAktien] = useState([])
 
     const requestHandler = new RequestHandler()
 
@@ -24,46 +25,50 @@ function AktienAuswahlModal(props) {
         });
     }, [])
 
-    function onSubmitHandler(event) {
-        // event.preventDefault();
-        // const requestBody = {
-        //     app_name: appName,
-        //     api_name: apiName,
-        // }
-        // props.onSubmit(requestBody)
+    function aktienHinzufuegen(event) {
+        event.preventDefault();
+        const requestBody = {
+            app_name: "appName",
+            api_name: "apiName",
+        }
+        props.onSubmit(requestBody)
     }
 
-    // function apiNameOnChangeHandler(event) {
-    //     setApiName(event.target.value)
-    // }
-    //
-    // function appNameOnChangeHandler(event) {
-    //     setAppName(event.target.value)
-    // }
+    function toggleSelectedAktie(event, name) {
+        let selectedAktie = new SelectedAktie(name);
+        if (event.target.checked) {
+            setSelectedAktien(prevSelectedAktien => {
+                return [...prevSelectedAktien, selectedAktie]
+            })
+        } else {
+            setSelectedAktien(prevSelectedAktien => {
+                return prevSelectedAktien.filter(item => item.name !== selectedAktie.name)
+            })
+        }
+    }
 
     return (
         <div>
             <Dialog open={props.open} onClose={props.onClose} wi>
-                <form onSubmit={onSubmitHandler}>
+                <form onSubmit={aktienHinzufuegen}>
 
                     <DialogTitle>Aktien zu Fundamentalanalyse hinzufügen</DialogTitle>
                     <DialogContent>
                         <TableContainer component={Paper}
                                         sx={{width: 1, margin: "auto"}} center>
                             <Table sx={{minWidth: 300,}} aria-label="simple table">
-                                <TableHead>
-                                    <TableRow>
-                                        <TableCell align="center">Hinzufügen</TableCell>
-                                        <TableCell align="center">Aktie</TableCell>
-                                    </TableRow>
-                                </TableHead>
                                 <TableBody>
                                     {aktienAuswahl.map((aktie) =>
                                         <TableRow key={aktie.name}>
                                             <TableCell align="center">
-                                                <Checkbox defaultChecked/>
+                                                <FormGroup>
+                                                    <FormControlLabel
+                                                        label={aktie.name}
+                                                        onChange={e => toggleSelectedAktie(e, aktie.name)}
+                                                        control={<Checkbox/>}
+                                                    />
+                                                </FormGroup>
                                             </TableCell>
-                                            <TableCell align="center">{aktie.name}</TableCell>
                                         </TableRow>)
                                     }
                                 </TableBody>
---
+++ src/aktien/structs/AktienStructs.js
@@ -0,0 +1,11 @@
+class SelectedAktie {
+
+    name = ""
+
+    constructor(name) {
+        this.name = name
+    }
+
+}
+
+export default SelectedAktie;

2eb193fd894ca19e7a29122c1e4e65ac8475c3bb
TehseenRug <tehseen.rug@gmail.com>
2022-05-23 19:09:03 +0200

    introduce dummy tilegrid

--- src/aktien/AktienComponent.js
+++ src/aktien/AktienComponent.js
@@ -1,6 +1,6 @@
 import React, {useContext, useState} from "react";
 import AktienContext from "../aktien/data-store/AktienContext";
-import {Box} from "@mui/material";
+import {Box, ImageList, ImageListItem} from "@mui/material";
 import Button from "@mui/material/Button";
 import AktienAuswahlModal from "./aktien-modal/AktienAuswahlModal";
 
@@ -8,6 +8,8 @@ import AktienAuswahlModal from "./aktien-modal/AktienAuswahlModal";
 function AktienComponent() {
 
     const [modalOpen, setModalOpen] = useState(false);
+
+    const dummyList = ['a', 'b', 'c', 'd', 'e', 'f']
 
     const aktienContext = useContext(AktienContext)
 
@@ -20,14 +22,27 @@ function AktienComponent() {
     };
 
     return (
-        <Box display="flex" justifyContent="center" mb={3} mt={3}>
-            <Button variant="outlined" onClick={openAktienAuswahl}>
-                Aktien hinzufügen
-            </Button>
-            <AktienAuswahlModal onSubmit={aktienContext.auswahlBestaetigen}
-                                open={modalOpen}
-                                onClose={closeAktienAuswahl}/>
-        </Box>
+        <div>
+            <Box display="flex" justifyContent="center" mb={3} mt={3}>
+                <div>
+                    <Button variant="outlined" onClick={openAktienAuswahl}>
+                        Aktien hinzufügen
+                    </Button>
+                    <AktienAuswahlModal onSubmit={aktienContext.auswahlBestaetigen}
+                                        open={modalOpen}
+                                        onClose={closeAktienAuswahl}/>
+                </div>
+            </Box>
+            <Box display="flex" justifyContent="center" mb={3} mt={3}>
+                <ImageList sx={{width: 500, height: 450}} cols={3} rowHeight={164}>
+                    {dummyList.map((aktie) => (
+                        <ImageListItem key={aktie}>
+                            <Button>{aktie}</Button>
+                        </ImageListItem>
+                    ))}
+                </ImageList>
+            </Box>
+        </div>
     );
 }
 
